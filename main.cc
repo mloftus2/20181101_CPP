02ff9e29bdb2e5b3bc45a8049f6ebc8436b96354
Mike Loftus <github@loftusoft.com>
2018-11-12 16:58:13 -0500

    forgot to add main.cc

--- main.cc
+++ main.cc
@@ -1,8 +1,13 @@
+//
+// c++ -o main main.cc
+//
+#include "common.h"
 #include <iostream>
 using namespace std;
 
 int main(int argc, char **argv)
 {
 	cout << "hello world" << endl;
+	cout << "magic number is " << MAGIC_NUMBER << endl;
 	return 0;
 }
